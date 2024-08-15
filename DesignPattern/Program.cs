35f844130af53ce4cc1afdaed8738a28f67f3ed7
prandeepchoudhary <prandeep28@outlook.com>
2024-08-15 20:45:45 +0530

    prince code for company work

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -4,96 +4,98 @@ class Program
 {
     static void Main(string[] args)
     {
+        #region "Prince company Code"
+        //List<ProductClass> lstFinalProduct = new List<ProductClass>();
+        //List<ProductClass> lstProduct = new List<ProductClass>();
 
-        List<ProductClass> lstFinalProduct = new List<ProductClass>();
-        List<ProductClass> lstProduct = new List<ProductClass>();
+        //lstProduct.Add(new ProductClass()
+        //{
+        //   name = "saving account",
+        //   price = "100",
+        //   productCVarients = new List<ProductCVarient>()
+        //   {
+        //       new ProductCVarient()
+        //   {
+        //           varientId = 1,
+        //           varientName = "test",
 
-        lstProduct.Add(new ProductClass()
-        {
-           name = "saving account",
-           price = "100",
-           productCVarients = new List<ProductCVarient>()
-           {
-               new ProductCVarient()
-           {
-                   varientId = 1,
-                   varientName = "test",
+        //   },
+        //    new ProductCVarient()
+        //    {
+        //        varientId = 2,
+        //        varientName = "test2"
+        //    }
+        //    ,
+        //    new ProductCVarient()
+        //    {
+        //        varientId = 7,
+        //        varientName = "test2"
+        //    }
+        //    ,
+        //    new ProductCVarient()
+        //    {
+        //        varientId = 10,
+        //        varientName = "test2"
+        //    }
 
-           },
-            new ProductCVarient()
-            {
-                varientId = 2,
-                varientName = "test2"
-            }
-            ,
-            new ProductCVarient()
-            {
-                varientId = 7,
-                varientName = "test2"
-            }
-            ,
-            new ProductCVarient()
-            {
-                varientId = 10,
-                varientName = "test2"
-            }
+        //   }
+        //});
 
-           }
-        });
+        //lstProduct.Add(new ProductClass()
+        //{
+        //    name = "insurance ",
+        //    price = "100",
+        //    productCVarients = new List<ProductCVarient>()
+        //   {
+        //       new ProductCVarient()
+        //   {
+        //           varientId = 3,
+        //           varientName = "test",
 
-        lstProduct.Add(new ProductClass()
-        {
-            name = "insurance ",
-            price = "100",
-            productCVarients = new List<ProductCVarient>()
-           {
-               new ProductCVarient()
-           {
-                   varientId = 3,
-                   varientName = "test",
+        //   },
+        //    new ProductCVarient()
+        //    {
+        //        varientId = 5,
+        //        varientName = "test2"
+        //    }
 
-           },
-            new ProductCVarient()
-            {
-                varientId = 5,
-                varientName = "test2"
-            }
+        //   }
+        //});
 
-           }
-        });
+        //List<int> lstVar = new List<int>();
+        //lstVar.Add(1);
+        //lstVar.Add(2);
 
-        List<int> lstVar = new List<int>();
-        lstVar.Add(1);
-        lstVar.Add(2);
-
-        foreach(var item in lstProduct)
-        {
-            List<ProductCVarient> lstvarTemp = new List<ProductCVarient>();
-            foreach(int varient in lstVar)
-            {
-                var result = item.productCVarients.Where(a => a.varientId == varient).ToList();
-                if(result != null && result.Count > 0)
-                {
-                    lstvarTemp.AddRange(result);
+        //foreach(var item in lstProduct)
+        //{
+        //    List<ProductCVarient> lstvarTemp = new List<ProductCVarient>();
+        //    foreach(int varient in lstVar)
+        //    {
+        //        var result = item.productCVarients.Where(a => a.varientId == varient).ToList();
+        //        if(result != null && result.Count > 0)
+        //        {
+        //            lstvarTemp.AddRange(result);
                     
-                }
-            }
+        //        }
+        //    }
 
-            if(lstvarTemp != null && lstvarTemp.Count > 0)
-            {
-                lstFinalProduct.Add(new ProductClass()
-                {
-                    name = item.name,
-                    price = item.price,
-                    productCVarients = lstvarTemp
-                });
-            }
+        //    if(lstvarTemp != null && lstvarTemp.Count > 0)
+        //    {
+        //        lstFinalProduct.Add(new ProductClass()
+        //        {
+        //            name = item.name,
+        //            price = item.price,
+        //            productCVarients = lstvarTemp
+        //        });
+        //    }
 
-            if(lstvarTemp.Count() == lstVar.Count())
-            {
-                break;
-            }
-        }
+        //    if(lstvarTemp.Count() == lstVar.Count())
+        //    {
+        //        break;
+        //    }
+        //}
+
+        #endregion
         Testing testing = new Testing();
         //string[] words = { "apple", "banana", "apple", "orange", "banana", "apple" };
         //string mostFrequentWord = testing.FindMostFrequentWord(words);
