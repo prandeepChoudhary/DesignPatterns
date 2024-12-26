4ee10546167fba903fefc1a5eb74c19223d641ef
prandeepchoudhary <prandeep28@outlook.com>
2024-12-26 14:01:36 +0530

    last file

---
+++ DesignPattern/Array/InterfaceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DesignPattern.Array
+{
+	public class InterfaceTest : IA, IB
+	{
+		void IA.GetTest()
+		{
+		}
+		void IB.GetTest()
+		{
+		}
+	}
+
+	public interface IA
+	{
+		void GetTest();
+	}
+
+    public interface IB
+    {
+        void GetTest();
+    }
+}
+
